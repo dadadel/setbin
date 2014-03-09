42634596f87ba35b41200509583ea68b4f1b274a
dadadel <daouzli@gmail.com>
2014-03-09 21:07:11 +0100

    replace global vars by dynamic memory allocation

--- setbin.c
+++ setbin.c
@@ -2,23 +2,27 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define MAX_COUPLES_OFFSET_VALUE 200
 
-unsigned long offsets[MAX_COUPLES_OFFSET_VALUE];
-char values[MAX_COUPLES_OFFSET_VALUE];
-int nb;
+typedef struct _bin_couples_t
+{
+    int nb;
+    unsigned long *offsets;
+    char *values;
+} bin_couples_t;
 
-char change(offset, value)
+char change(unsigned long offset, char value, bin_couples_t *cpl)
 {
     int i;
     char c = value;
 
-    for (i = 0; i < nb; i++)
+    for (i = 0; i < cpl->nb; i++)
     {
-        if (offset == offsets[i])
+        if (offset == cpl->offsets[i])
         {
-            c = values[i];
+            c = cpl->values[i];
             break;
         }
     }
@@ -31,6 +35,7 @@ int main(int argc, char **argv)
     unsigned long i;
     char c;
     int fs, fd, ret;
+    bin_couples_t cpl;
 
     if (argc < 5)
     {
@@ -41,12 +46,23 @@ int main(int argc, char **argv)
     src = argv[1];
     dst = argv[2];
 
-    nb = (argc - 3) / 2;
-    nb = nb < MAX_COUPLES_OFFSET_VALUE ? nb : MAX_COUPLES_OFFSET_VALUE;
-    for (i = 0; i < nb; i++)
+    cpl.nb = (argc - 3) / 2;
+    cpl.offsets = (unsigned long *)malloc(sizeof(unsigned long) * cpl.nb);
+    if (cpl.offsets == NULL)
     {
-        offsets[i] = atol(argv[3 + (i * 2)]);
-        values[i] = atoi(argv[4 + (i * 2)]);
+        printf("Memory allocation failed\n");
+        return 0;
+    }
+    cpl.values = (char *)malloc(sizeof(char) * cpl.nb);
+    if (cpl.values == NULL)
+    {
+        printf("Memory allocation failed\n");
+        return 0;
+    }
+    for (i = 0; i < cpl.nb; i++)
+    {
+        cpl.offsets[i] = atol(argv[3 + (i * 2)]);
+        cpl.values[i] = atoi(argv[4 + (i * 2)]);
     }
 
     fs = open(src, O_RDONLY);
@@ -73,7 +89,7 @@ int main(int argc, char **argv)
             printf("nothing read (offset 0x%lX)\n", i);
             break;
         }
-        c = change(i, c);
+        c = change(i, c, &cpl);
         ret = write(fd, &c, 1);
         if (ret < 1)
         {
@@ -82,7 +98,8 @@ int main(int argc, char **argv)
         }
         i++;
     }
-
+    free(cpl.offsets);
+    free(cpl.values);
     close(fs);
     close(fd);
     return 0;
